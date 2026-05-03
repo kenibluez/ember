28e308b90e60541bce545159ce5a24e274ba1eac
keniher <kenneyher@gmail.com>
2026-05-03 12:51:02 -0600

    feat(ui): create color palette and initialize qss file

---
+++ app/styles/theme.py
@@ -0,0 +1,13 @@
+class Colors:
+    # Backgrounds
+    BACKGROUND    = "#111111"
+    SURFACE       = "#1C1C1C"
+    COMPONENTS    = "#2B2B2B"
+
+    # brand
+    PRIMARY       = "#FF6524"
+    SECONDARY     = "#E8311A"
+
+    # Text
+    FOREGROUND    = "#F2F2F2"
+    MUTED_FG      = "#888888" 
